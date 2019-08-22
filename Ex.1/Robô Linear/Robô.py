8abacfcece8746051ffa2fcb9e873877b5f1c594
Nathan Freitas <maxtan_11@hotmail.com>
2019-08-22 14:14:03 -0300

    Adicionando novos comentários

--- Ex.1/Robô Linear/Robô.py
+++ Ex.1/Robô Linear/Robô.py
@@ -1,9 +1,10 @@
 Passos = input("Digite a lista de comandos do robô: ").upper()
+#Verificar se tem a string "TF" e a coloca como string vazia
 while "TF" in Passos:
     Passos = Passos.replace("TF","")
+#Verificar se tem a string "FT" e a coloca como string vazia
 while "FT" in Passos:
     Passos = Passos.replace("FT","")
-print(Passos) #Aqui está os passos sem está sendo contado
 if len(Passos)>0:
     print("O robô deu %d passos em relação ao ponto inicial."%len(Passos)) #Aqui faz uma contagem de quantos passos ele deu direto para o percusso
 else:
